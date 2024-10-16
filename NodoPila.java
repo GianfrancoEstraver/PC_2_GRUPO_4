2df3d96ba0146ae8652e08ee1ed1f889ce3551b9
Juan Urbano <168696028+Zack4Logan@users.noreply.github.com>
2024-10-16 16:44:01 -0500

    Add files via upload

---
+++ NodoPila.java
@@ -0,0 +1,13 @@
+package MiAporte;
+
+// @author 7zacg
+public class NodoPilaU {
+
+    String tarea;
+    NodoPilaU siguiente;
+
+    NodoPilaU(String tarea) {
+        this.tarea = tarea;
+        this.siguiente = null;
+    }
+}
